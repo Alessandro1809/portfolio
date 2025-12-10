394f985308faddba3ff012bc3f2fa58bfc37f55f
nous-adg <alessandro@nous.cr>
2025-12-10 01:29:35 -0600

    primer commit, estado estable, pendiente el blog

---
+++ src/components/react/TechStack.tsx
@@ -0,0 +1,25 @@
+
+ import {
+  AnimatedSpan,
+  Terminal,
+  TypingAnimation,
+} from "@/components/react/Terminal"
+
+export const TechStack = () => {
+  return (
+   
+<Terminal>
+  <TypingAnimation>My Tech Stack</TypingAnimation>
+  <AnimatedSpan>✔ Typescript</AnimatedSpan>
+  <AnimatedSpan>✔ React.</AnimatedSpan>
+  <AnimatedSpan>✔ Astro.</AnimatedSpan>
+  <AnimatedSpan>✔ Tailwind CSS.</AnimatedSpan>
+  <AnimatedSpan>✔ Next.js.</AnimatedSpan>
+  <AnimatedSpan>✔ Node.js.</AnimatedSpan>
+  <AnimatedSpan>✔ MongoDB.</AnimatedSpan>
+  <AnimatedSpan>✔ PostgreSQL.</AnimatedSpan>
+  <AnimatedSpan>✔ SQLite.</AnimatedSpan>
+  <TypingAnimation>Everything is possible with code!</TypingAnimation>
+</Terminal>
+  )
+}
