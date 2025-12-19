72c65a99dc9ef1c56eff0da2660090fc4164f1d8
nous-adg <alessandro@nous.cr>
2025-12-18 23:29:25 -0600

    cambiando locals

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,18 +1,14 @@
-/// <reference types="astro/client" />
-import type { Client } from "@libsql/client/web";
+export interface Env {
 
-declare global {
-    namespace App {
-        interface Locals {
-            TURSO_DB_URL: string;
-            TURSO_AUTH_TOKEN: string;
-            turso: Client;
-            runtime: import("@astrojs/cloudflare").Runtime<Env>;
-        }
-    }
-}
-
-interface Env {
     TURSO_DB_URL: string;
     TURSO_AUTH_TOKEN: string;
+
+}
+
+declare module 'astro' {
+    interface Locals {
+        runtime: {
+            env: Env;
+        };
+    }
 }
