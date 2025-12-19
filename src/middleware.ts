eb48489d4998c811073e5a2e129361cae2ab90fb
nous-adg <alessandro@nous.cr>
2025-12-18 22:33:09 -0600

    test of secrets and clien/web

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from "astro/middleware";
 import { createClient } from "@libsql/client/web";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-    context.locals.db = createClient({
+    context.locals.turso = createClient({
         url: context.env.TURSO_DB_URL,
         authToken: context.env.TURSO_AUTH_TOKEN,
     });
