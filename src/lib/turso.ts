8c6aa30277bf388f98232513fdbcff9772a5d55c
nous-adg <alessandro@nous.cr>
2025-12-18 23:15:48 -0600

    test of secrets and clien/web

--- src/lib/turso.ts
+++ src/lib/turso.ts
@@ -1,15 +1,8 @@
-import { createClient } from "@libsql/client/web";
+import { createClient, type Client } from '@libsql/client/web';
 
-export default {
-    async fetch(req, env) {
-        const db = createClient({
-            url: env.TURSO_DB_URL,
-            authToken: env.TURSO_AUTH_TOKEN,
-        });
-
-        // test real
-        return db.execute("SELECT 1")
-            .then(() => new Response("OK"))
-            .catch(err => new Response(err.message));
-    }
-};
+export function createDbClient(env: { TURSO_DB_URL: string; TURSO_AUTH_TOKEN?: string }): Client {
+    return createClient({
+        url: env.TURSO_DB_URL,
+        authToken: env.TURSO_AUTH_TOKEN,
+    });
+}
